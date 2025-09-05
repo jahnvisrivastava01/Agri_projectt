0df24c720f3a6274eae637c6e20719be9d633a51
Jahnvi Srivastava <jahnvisrivastava0104@gmail.com>
2025-09-05 16:28:52 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -182,7 +182,9 @@ def dashboard():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Render's assigned port, default to 5000 locally
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 
